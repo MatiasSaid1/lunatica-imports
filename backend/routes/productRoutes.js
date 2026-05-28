6afeb68301f789c3e7b3e539f5f62d3fc60aa021
Matias Said <matysaid22@gmail.com>
2026-05-27 21:03:09 -0300

    temporary middleware remane

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const Product = require("../models/Product");
 
-const authMiddleware = require("../middleware/authMiddleware");
+const authMiddleware = require("../middleware/TEMP_AUTH");
 
 router.get("/", async (req, res) => {
   try {
